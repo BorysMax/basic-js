46d58371cd5b397698dd7afc6b92aeaf6935e497
Borys Maksymov <radiatorzp@gmail.com>
2022-10-14 11:34:01 +0300

    feat: add one more tasks

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
+ function getCommonCharacterCount(s1, s2) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let res = 0;
+  let str = s2;
+  let count = 0;
+  console.log(s1.length);
+  for (let i = 0; i < s1.length; i++) {
+      for (let j = 0; j < str.length; j++) {
+          if (s1[i] === str[j]) {
+              count++;
+              str = str.slice(0, j) + str.slice(j+1, str.length);
+              break;
+          }
+      }
+  }
+  return count;
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+ function transform(arr) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!(arr instanceof Array)) throw new Error ("'arr' parameter must be an instance of the Array!");
+  const action = [
+  '--discard-next',
+  '--discard-prev',
+  '--double-next',
+  '--double-prev'];
+  let arrRes = [];
+  let i = 0;
+  while (i < arr.length) {
+    arrRes.push(arr[i]);
+      for (let j = 0; j < action.length; j++) {
+        if (arr[i] === action[0]) {
+            arrRes.pop();
+            if (i !== arr.length-1) i ++;
+            if (arr[i+1] === action[1] || arr[i+1] === action[3]) i++;
+            break;
+        }
+        if (arr[i] === action[1]) {
+            arrRes.pop();
+            if (i !== 0) arrRes.pop();
+            break;            
+        }    
+        if (arr[i] === action[2]) {
+            arrRes.pop();
+            if (i !== arr.length-1) arrRes.push(arr[i+1]);
+            break;
+        }
+        if (arr[i] === action[3]) {
+            arrRes.pop();
+            if (i !== 0) arrRes.push(arrRes[arrRes.length-1]);
+            break;
+        }    
+      }
+    i++;
+  }        
+return arrRes;
 }
 
 module.exports = {
