408866e8b4df85fdb8db6b526684040fe346ca3b
Borys Maksymov <radiatorzp@gmail.com>
2023-04-14 11:27:40 +0300

    feat: add one more tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,73 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+
+  encrypt(message, key) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    const alphabet = this.alphabet;
+    const msgUp = message.toUpperCase();
+    const kUp = key.toUpperCase();
+    const len = alphabet.length;
+
+    let res = '';
+    let index = 0;
+
+    for (let i = 0; i < message.length; i++) {
+    const msgChar = msgUp[i];
+    const msgCharIndex = alphabet.indexOf(msgChar);
+
+    if (msgCharIndex === -1) {
+      res += msgChar;
+    } else {
+      const kChar = kUp[index % kUp.length];
+      const kCharIndex = alphabet.indexOf(kChar);
+
+      const encCharIndex = (msgCharIndex + kCharIndex) % len;
+      const encChar = alphabet[encCharIndex];
+
+      res += encChar;
+      index++;
+    }
+  }
+    return this.isDirect ? res : res.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    const alphabet = this.alphabet;
+    const msgUp = message.toUpperCase();
+    const kUp = key.toUpperCase();
+    const len = alphabet.length;
+
+    let res = '';
+    let index = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const encChar = msgUp[i];
+      const encCharIndex = alphabet.indexOf(encChar);
+      if (encCharIndex === -1) {
+        res += encChar;
+      } else {
+        const kChar = kUp[index % kUp.length];
+        const kCharIndex = alphabet.indexOf(kChar);
+        const msgCharIndex = (encCharIndex - kCharIndex + len) % len;
+        const msgChar = alphabet[msgCharIndex];
+        res += msgChar;
+        index++;
+      }
+    }
+    return this.isDirect ? res : res.split('').reverse().join('');
   }
 }
 
